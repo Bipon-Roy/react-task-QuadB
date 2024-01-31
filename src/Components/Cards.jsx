a0b6ac9a8bf33eb3c33cea8eafc5f7aa021e6a99
Bipon Roy <biponroy400@gmail.com>
2024-02-01 03:14:04 +0600

    Updated cards design

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -3,23 +3,43 @@ import PropTypes from "prop-types";
 
 //Recipe Card
 const Cards = ({ show }) => {
-    const { id, name, image, status } = show.show; //destructuring properties
-    console.log(status);
+    const { id, name, image, status, premiered, ended, genres } = show.show; //destructuring properties
+    console.log(show.show);
     return (
         <div className="flex flex-col shadow rounded-md">
             <div className="flex justify-center">
                 <img
-                    className="w-full object-fill h-[300px] rounded-t-md"
+                    className="w-full object-fill h-[250px] rounded-t-md"
                     src={image?.medium}
                     alt={name}
                 />
             </div>
 
-            <div className="flex-grow mt-3  text-sm md:text-base px-4">
+            <div className="flex-grow mt-3 space-y-2 text-sm md:text-base px-4">
+                <div className="flex gap-1">
+                    <span className="font-medium">Genres: </span>
+                    {genres.map((item, idx) => (
+                        <p
+                            key={idx}
+                            className="max-w-fit px-2 bg-blue-500 text-white rounded-3xl flex items-center text-sm"
+                        >
+                            {item}
+                        </p>
+                    ))}
+                </div>
+                <p className="font-medium">
+                    <span>Premiered: </span>
+                    {premiered ? premiered : "N/A"}
+                </p>
+                <p className="font-medium">
+                    <span>Ended: </span>
+                    {ended ? ended : "N/A"}
+                </p>
+
                 <p
                     className={`${
-                        status === "Ended" ? "text-red-600" : "text-green-600"
-                    } font-medium`}
+                        status === "Ended" ? "text-red-600" : "text-green-500"
+                    } font-semibold`}
                 >
                     <span className="text-black">Status: </span>
                     {status}
@@ -27,7 +47,7 @@ const Cards = ({ show }) => {
             </div>
             <div className="px-4 my-3">
                 <Link to={`/showDetails/${id}`}>
-                    <button className="py-1 bg-purple-600 text-white w-full rounded-md font-medium">
+                    <button className="py-1 bg-[#ba181b] text-white w-full rounded-md font-medium">
                         See Details
                     </button>
                 </Link>
